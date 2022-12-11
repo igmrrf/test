a00af301c2c4e405bd48d57c0d9acf063d3fbf69
Igbiriki Francis Richard <francis@veendhq.com>
2022-12-11 22:45:34 +0100

    eye

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const test = 'Testing the merging abilities';
 const rebase = 'this is a rebase commit';
 const integration2 =
   "This is update on integration that doesn't exist on main while going to merges";
-const integration = 'This is an integration commit';
+const integration = 'This is an integration commit conflict me';
 
-const hotfix = 'This is a hot fix commit';
+const hotfix = 'This is a hot fix commit conflict me';
 
 const nextLevelIntegrations = 'This is a next level integration';
 
 const online = 'This is also an online commit';
+const eye = 'This is an eye conflict';
