7493c2f0714e2e067018c4f185bdc4b2c03a5a13
Igbiriki Francis Richard <francis@veendhq.com>
2022-12-11 21:14:42 +0100

    rebase

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const test = 'Testing the merging abilities';
+const rebase = 'this is a rebase commit';
 const integration2 =
   "This is update on integration that doesn't exist on main while going to merges";
 const integration = 'This is an integration commit';
